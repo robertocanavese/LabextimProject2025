a96cad78801899dddf936144e04b8a840db0ad7a
robertocanavese <roberto.canavese@tiscali.it>
2026-04-09 13:02:55 +0200

    a

--- LabExtim/AutoAuth.aspx.cs
+++ LabExtim/AutoAuth.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DLLabExtim;
 using UILabExtim;
 
 namespace LabExtim
@@ -14,10 +15,29 @@ namespace LabExtim
         {
             if (!string.IsNullOrWhiteSpace(Request.QueryString["tkn"]))
             {
+                int idUser = -1;
+                using (var _qc = new QuotationDataContext())
+                {
+                    Token found = _qc.Tokens.FirstOrDefault(d => d.IdToken == Request.QueryString["tkn"]);
+                    if (found != null)
+                    {
+                        Uri uri = new Uri(found.RedirectUrl);
 
+                        if (uri.AbsoluteUri.ToLower().Contains("LeaveRequestsConsole".ToLower()))
+                        {
+                            int idLeaveRequest = Convert.ToInt32(HttpUtility.ParseQueryString(uri.Query).Get("toauthid"));
+                            new ProductionOrderDetailsInsertController().ChangeLeaveRequestStatus(20, idLeaveRequest, found.IdUser);
+                            lblMessage.Text = string.Format("Richiesta permesso No. {0} autorizzata con successo!", idLeaveRequest);
+                        }
+                        _qc.Tokens.DeleteOnSubmit(found);
+                        _qc.SubmitChanges();
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Autoautenticazione fallita!";
+                    }
+                }
 
-
-                new ProductionOrderDetailsInsertController().ChangeLeaveRequestStatus(20, Convert.ToInt32(e.CommandArgument), WebUser.Employee.ID);
 
             }
 
